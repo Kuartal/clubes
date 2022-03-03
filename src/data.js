fa3da138040ca0fadcd03806ac927da9371d507a
Kuartal <ratg2001@gmail.com>
2022-03-03 14:00:59 +0000

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,118 @@
+module.exports = {
+    Accounts: [
+      {
+        '_id': '0',
+        'Nome': 'Sporting',
+        'Morada': 'Rua Professor Fernando da Fonseca Apartado 4120, 1501-806 Lisboa',
+      },
+        {
+          '_id': '1',
+          'Nome': 'Benfica',
+          'Morada': 'Estádio do Sport Lisboa e Benfica - Porta 18 Av. Eusébio da Silva Ferreira, 1500-313',
+        },
+        {
+          '_id': '2',
+          'Nome': 'Porto',
+          'Morada': '507 Church Avenue, Heil, Wyoming, 1754'
+        },
+        {
+          '_id': '3',
+          'Nome': 'Chaves',
+          'Morada': 'Estádio Municipal Engº Manuel Branco Teixeira Av. do Estádio Municipal 5400-234 Chaves',
+        },
+        {
+          '_id': '4',
+          'Nome': 'Paços de Ferreira',
+          'Morada': 'Rua do Estádio 95, 4590-571 Paços de Ferreira Portugal'
+        }
+    ],
+    Cars: [
+    {
+        '_id': 0,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 1,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 2,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 3,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 4,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 5,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 6,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 7,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 8,
+        'Jogadores': 'chevrolet chevelle malibu',
+    },
+    {
+        '_id': 9,
+        'Jogadores': 'chevrolet chevelle malibu',
+    }
+  ], 
+  Accounts_Cars: [
+    {
+        '_id': 0,
+        'Account_id': 0,
+        'Car_id': 0
+    },
+    {
+        '_id': 1,
+        'Account_id': 0,
+        'Car_id': 1
+    },
+    {
+        '_id': 2,
+        'Account_id': 1,
+        'Car_id': 2
+    },
+    {
+        '_id': 3,
+        'Account_id': 3,
+        'Car_id': 3
+    },
+    {
+        '_id': 4,
+        'Account_id': 3,
+        'Car_id': 5
+    },
+    {
+        '_id': 5,
+        'Account_id': 3,
+        'Car_id': 7
+    },
+    {
+        '_id': 6,
+        'Account_id': 4,
+        'Car_id': 6
+    },
+    {
+        '_id': 7,
+        'Account_id': 4,
+        'Car_id': 4
+    },
+    {
+        '_id': 8,
+        'Account_id': 4,
+        'Car_id': 8
+    }
+  ]
+}
---
+++ start-sample.sh
@@ -0,0 +1,3 @@
+## start locally
+node src/index.js --service clubes --port 8080 --key sslcert/server.key --cert sslcert/server.cert -v
+
